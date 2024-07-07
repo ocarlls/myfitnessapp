19e19d7176574a9c784082ff0b34dd0f13024c73
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2024-07-07 15:47:46 -0300

    refact: Select just most relevant food nutrients to be deserialized from response json

--- user/src/main/java/com/ms/user/models/Serving.java
+++ user/src/main/java/com/ms/user/models/Serving.java
@@ -6,46 +6,28 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Serving {
-    @JsonProperty("calcium")
-    private String calcium;
+    @JsonProperty("serving_id")
+    private String servingId;
     @JsonProperty("calories")
     private String calories;
     @JsonProperty("carbohydrate")
     private String carbohydrate;
-    @JsonProperty("cholesterol")
-    private String cholesterol;
     @JsonProperty("fat")
     private String fat;
+    @JsonProperty("protein")
+    private String protein;
     @JsonProperty("fiber")
     private String fiber;
-    @JsonProperty("iron")
-    private String iron;
     @JsonProperty("measurement_description")
     private String measurementDescription;
     @JsonProperty("metric_serving_amount")
     private String metricServingAmount;
     @JsonProperty("metric_serving_unit")
     private String metricServingUnit;
-    @JsonProperty("monounsaturated_fat")
-    private String monounsaturatedFat;
     @JsonProperty("number_of_units")
     private String numberOfUnits;
-    @JsonProperty("polyunsaturated_fat")
-    private String polyunsaturatedFat;
-    @JsonProperty("potassium")
-    private String potassium;
-    @JsonProperty("protein")
-    private String protein;
     @JsonProperty("saturated_fat")
     private String saturatedFat;
     @JsonProperty("serving_description")
     private String servingDescription;
-    @JsonProperty("sodium")
-    private String sodium;
-    @JsonProperty("sugar")
-    private String sugar;
-    @JsonProperty("vitamin_a")
-    private String vitaminA;
-    @JsonProperty("vitamin_c")
-    private String vitaminC;
 }
